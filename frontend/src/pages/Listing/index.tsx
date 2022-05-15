d851b2c2cf4ed7f1fe2cbe99e39431570630ac6d
Danilo José <danilo.j.stos@gmail.com>
2022-05-14 23:05:40 -0300

    props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,45 +1,47 @@
-import axios from 'axios';
+import axios from 'axios'
 import MovieCard from 'components/MovieCard'
 import Pagination from 'components/Pagination'
-import { useEffect, useState } from 'react';
-import { MoviePage } from 'types/movie';
-import { BASE_URL } from 'utils/requests';
+import { useEffect, useState } from 'react'
+import { MoviePage } from 'types/movie'
+import { BASE_URL } from 'utils/requests'
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0)
 
-  const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true
+  })
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-      .then(resp => {
-        const data = resp.data as MoviePage;
-        setPageNumber(data.number);
-      });
-  }, []);
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then(resp => {
+      const data = resp.data as MoviePage
+      setPage(data)
+    })
+  }, [pageNumber])
 
   return (
     <>
-      <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-            <MovieCard />
-          </div>
+          {page.content.map(movie => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Listing;
+export default Listing
